27dcb0e7166b05a6868d6fca6c395afd7abee0f1
lichuan <lichuan@e-lead.cn>
2018-08-17 23:33:28 +0800

    lombok

--- SpringBoot-Dubbo-Docker-Jenkins-master/Gaoxi-Common-Service-Facade/src/main/java/com/gaoxi/entity/user/UserEntity.java
+++ SpringBoot-Dubbo-Docker-Jenkins-master/Gaoxi-Common-Service-Facade/src/main/java/com/gaoxi/entity/user/UserEntity.java
@@ -2,6 +2,7 @@ package com.gaoxi.entity.user;
 
 import com.gaoxi.enumeration.user.UserStateEnum;
 import com.gaoxi.enumeration.user.UserTypeEnum;
+import lombok.Data;
 
 import java.io.Serializable;
 import java.sql.Timestamp;
@@ -11,6 +12,7 @@ import java.util.List;
  * @Author 大闲人柴毛毛
  * @Date 2017/10/30 下午6:11
  */
+@Data
 public class UserEntity implements Serializable {
 
     /** 主键 */
@@ -42,102 +44,4 @@ public class UserEntity implements Serializable {
 
     /** 用户的角色 */
     private RoleEntity roleEntity;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getMail() {
-        return mail;
-    }
-
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
-
-    public String getLicencePic() {
-        return licencePic;
-    }
-
-    public void setLicencePic(String licencePic) {
-        this.licencePic = licencePic;
-    }
-
-    public Timestamp getRegisterTime() {
-        return registerTime;
-    }
-
-    public void setRegisterTime(Timestamp registerTime) {
-        this.registerTime = registerTime;
-    }
-
-    public UserTypeEnum getUserTypeEnum() {
-        return userTypeEnum;
-    }
-
-    public void setUserTypeEnum(UserTypeEnum userTypeEnum) {
-        this.userTypeEnum = userTypeEnum;
-    }
-
-    public UserStateEnum getUserStateEnum() {
-        return userStateEnum;
-    }
-
-    public void setUserStateEnum(UserStateEnum userStateEnum) {
-        this.userStateEnum = userStateEnum;
-    }
-
-    public RoleEntity getRoleEntity() {
-        return roleEntity;
-    }
-
-    public void setRoleEntity(RoleEntity roleEntity) {
-        this.roleEntity = roleEntity;
-    }
-
-
-
-    @Override
-    public String toString() {
-        return "UserEntity{" +
-                "id='" + id + '\'' +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", phone='" + phone + '\'' +
-                ", mail='" + mail + '\'' +
-                ", licencePic='" + licencePic + '\'' +
-                ", registerTime=" + registerTime +
-                ", userTypeEnum=" + userTypeEnum +
-                ", userStateEnum=" + userStateEnum +
-                ", roleEntity=" + roleEntity +
-                '}';
-    }
 }
